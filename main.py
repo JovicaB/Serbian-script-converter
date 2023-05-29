8f122f763b2403cf3850e792a7f38ec0c24cfcee
Jovica Bozic <jovicab@gmail.com>
2023-05-29 12:26:36 +0200

    dictionary_fix

--- main.py
+++ main.py
@@ -5,37 +5,44 @@ class SerbianScriptConverter:
                 'K': 'К', 'k': 'к', 'L': 'Л', 'l': 'л', 'M': 'М', 'm': 'м', 'N': 'Н', 'n': 'н', 'O': 'О', 'o': 'о',
                 'P': 'П', 'p': 'п', 'R': 'Р', 'r': 'р', 'S': 'С', 's': 'с', 'T': 'т', 't': 'т', 'U': 'У', 'u': 'у',
                 'V': 'В', 'v': 'в', 'Z': 'З', 'z': 'з', 'Š': 'Ш', 'š': 'ш', 'Đ': 'Ђ', 'đ': 'ђ', 'Č': 'Ч', 'č': 'ч',
-                'Ć': 'Ћ', 'ć': 'ћ', ' ': ' '}
+                'Ć': 'Ћ', 'ć': 'ћ', "Ž": "Ж", "ž": "ж"}
     SPECIAL_CHR_DICT = {'Nj': 'Њ', 'nj': 'њ', 'Lj': 'Љ', 'lj': 'љ', 'Dž': 'Џ', 'dž': 'џ'}
 
     def __init__(self, input_str):
         self.input_str = input_str
 
-    def lat_cyr(self):
-        final_text = []
+    def lat_to_cyr(self):
+        converted_text= []
         input_str = self.input_str + " "
         i = 0
         while i < len(self.input_str):
-            if input_str[i] + input_str[i + 1] in SerbianScriptConverter.SPECIAL_CHR_DICT:
-                final_text.append(SerbianScriptConverter.SPECIAL_CHR_DICT[input_str[i] + input_str[i + 1]])
+            if input_str[i] + input_str[i + 1] in self.SPECIAL_CHR_DICT:
+                converted_text.append(self.SPECIAL_CHR_DICT[input_str[i] + input_str[i + 1]])
                 i += 2
-            elif input_str[i] in SerbianScriptConverter.CHR_DICT:
-                final_text.append(SerbianScriptConverter.CHR_DICT[input_str[i]])
+            elif input_str[i] in self.CHR_DICT:
+                converted_text.append(self.CHR_DICT[input_str[i]])
                 i += 1
             else:
-                final_text.append(input_str[i])
+                converted_text.append(input_str[i])
                 i += 1
-        return ''.join(final_text)
+        return ''.join(converted_text)
 
-    def cyr_lat(self):
-        new_dict = {**SerbianScriptConverter.CHR_DICT, **SerbianScriptConverter.SPECIAL_CHR_DICT}
+    def cyr_to_lat(self):
+        new_dict = {**self.CHR_DICT, **self.SPECIAL_CHR_DICT}
         new_dict = {v: k for k, v in new_dict.items()}
-        final_text = ''.join(map(lambda x: new_dict.get(x, x), self.input_str))
-        return final_text
+        converted_text = ''.join(map(lambda x: new_dict.get(x, x), self.input_str))
+        return converted_text
 
     @property
     def convert_script(self):
-        if any(i in SerbianScriptConverter.CHR_DICT.values() for i in self.input_str):
-            return self.cyr_lat()
+        if any(char in self.CHR_DICT.values() for char in self.input_str):
+            return self.cyr_to_lat()
         else:
-            return self.lat_cyr()
+            return self.lat_to_cyr()
+
+
+
+str_lat = "Odredbe ovog zakona primenjuju se na zaposlene strane državljane i lica bez državljanstva koji rade kod poslodavca na teritoriji Republike Srbije, ako zakonom"
+str_cyr = "Одредбе овог закона примењују се на запослене који раде на територији Републике. Србије, код домаћег или страног правног, односно физичког лица "
+
+print(SerbianScriptConverter(str_cyr).convert_script)
